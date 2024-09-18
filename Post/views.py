62b132df1f37f570f25ceb58ad6985690f96c394
Milenka Rojas <mrojasgarnica1@gmail.com>
2024-09-17 23:51:45 -0400

    posts reacciones

--- Post/views.py
+++ Post/views.py
@@ -1,6 +1,8 @@
+from django.shortcuts import get_object_or_404, redirect
 from django.shortcuts import render,redirect
-from .models import Post
+from .models import Post, Coment, Reaction
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     posts = Post.objects.all()
@@ -18,3 +20,28 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, 'create.html', {'form': form})
+
+@login_required
+def add_reaction(request, post_id=None, coment_id=None):
+    reaction_type = request.POST.get('reaction_type')
+    user = request.user
+    
+    # Si es un post
+    if post_id:
+        post = get_object_or_404(Post, id=post_id)
+        reaction, created = Reaction.objects.get_or_create(user=user, post=post)
+        
+        # Actualizar o crear reacción
+        reaction.reaction_type = reaction_type
+        reaction.save()
+        
+    # Si es un comentario
+    elif coment_id:
+        coment = get_object_or_404(Coment, id=coment_id)
+        reaction, created = Reaction.objects.get_or_create(user=user, coment=coment)
+        
+        # Actualizar o crear reacción
+        reaction.reaction_type = reaction_type
+        reaction.save()
+    
+    return redirect('home')
