f9e0fdaaf45a3726d5dd67918875ef0f06cefb17
Nohemi C <choqueramireznohemi@gmail.com>
2024-09-20 18:13:38 -0400

    profile añádido

--- Profile/forms.py
+++ Profile/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import  Profile
-
+from Post.models import Post 
 
 class ProfileForm(ModelForm):
     class Meta:
@@ -11,3 +11,7 @@ class ProfileForm(ModelForm):
             "biography": forms.Textarea(attrs={"class": "form-control bg-primary"}),
             "avatar": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
         }
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['description', 'file']  # Los campos que se editarán
--- Profile/urls.py
+++ Profile/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path("profile/",views.profile,name="profile"),
     path("profile/edit", views.edit_profile, name="edit_profile"),
+    path('post/create/', views.post_create2, name='post_create2'),
     # Agrega aquí otras rutas de la aplicación
 ]
