0afbba50819b3b51971b0003a4af8b17f1e2b8e7
Nohemi C <choqueramireznohemi@gmail.com>
2024-09-20 09:23:11 -0400

    post del perfil en un card new-post

--- Profile/forms.py
+++ Profile/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import  Profile
+from Post.models import Post 
 
 
 class ProfileForm(ModelForm):
@@ -12,3 +13,33 @@ class ProfileForm(ModelForm):
             "avatar": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
            "cover_photo": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
         }
+
+# class PostForm(ModelForm):
+#     class Meta:
+#         model = Post
+#         fields = ['description', 'file']
+    
+#     def clean_file(self):
+#         #Este método se llama automáticamente cuando se valida el formulario, específicamente para el campo file.
+#         file = self.cleaned_data.get('file')
+#         description = self.cleaned_data.get('description')
+        
+#         if file:
+#             # Comprobar si es un video o una imagen
+#             if file.name.endswith(('.mp4', '.mov')) and description:
+#                 raise forms.ValidationError("No puedes subir una imagen y un video a la vez.")
+        
+#         return file
+
+#     def clean(self):
+#         cleaned_data = super().clean()
+#         description = cleaned_data.get('description')
+#         file = cleaned_data.get('file')
+
+#         # Validar que al menos uno de los campos esté lleno
+#         if not description and not file:
+#             raise forms.ValidationError("Debes ingresar texto o subir un archivo (imagen o video).")
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['description', 'file']  # Los campos que se editarán
--- Profile/static/js/profile.js
+++ Profile/static/js/profile.js
@@ -1,18 +1,51 @@
-const defaultFile = 'https://via.placeholder.com/150';
+// const defaultFile = 'https://via.placeholder.com/150';
 
-const file = document.getElementById( 'profile-img-preview' );
-const img = document.getElementById( 'profile-avatar' );
-file.addEventListener( 'change', e => {
-  if( e.target.files[0] ){
-    const reader = new FileReader( );
-    reader.onload = function( e ){
-      img.src = e.target.result;
-    }
-    reader.readAsDataURL(e.target.files[0])
-  }else{
-    img.src = defaultFile;
-  }
-} );
+// const file = document.getElementById( 'profile-img-preview' );
+// const img = document.getElementById( 'profile-avatar' );
+// file.addEventListener( 'change', e => {
+//   if( e.target.files[0] ){
+//     const reader = new FileReader( );
+//     reader.onload = function( e ){
+//       img.src = e.target.result;
+//     }
+//     reader.readAsDataURL(e.target.files[0])
+//   }else{
+//     img.src = defaultFile;
+//   }
+// } );
+
+// document.getElementById('postSubmitLink').addEventListener('click', function(event) {
+//   event.preventDefault(); // Evita que el enlace haga su comportamiento por defecto
+//   document.getElementById('postForm').submit(); // Envía el formulario
+// });
 
 
-  
+
+
+
+
+// reacciones 
+// const reactionContainer = document.getElementById('reaction-container');
+//         const emojiContainer = document.getElementById('emoji-container');
+//         let showTimeout;
+
+//         // Muestra los emojis al pasar el cursor sobre el botón de reacción
+//         reactionContainer.addEventListener('mouseover', function() {
+//             clearTimeout(showTimeout); // Cancela el ocultamiento si el usuario vuelve a pasar el cursor
+//             emojiContainer.style.display = 'flex';
+//         });
+
+//         // Oculta los emojis si el cursor sale del área de los emojis y el botón de reacción
+//         reactionContainer.addEventListener('mouseleave', function() {
+//             showTimeout = setTimeout(function() {
+//                 emojiContainer.style.display = 'none';
+//             }, 300); // Un pequeño retraso para evitar que desaparezca al moverse rápidamente
+//         });
+
+//         // Función para seleccionar la reacción
+//         function selectReaction(reaction) {
+//             const reactionText = document.getElementById('reaction-text');
+//             reactionText.innerHTML = `<i class="far fa-thumbs-up"></i> ${reaction}`;
+//             reactionText.classList.add('reaction-selected');
+//             emojiContainer.style.display = 'none'; // Ocultar los emojis después de seleccionar
+//         }
